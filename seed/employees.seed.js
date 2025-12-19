27bc0b05f53012f0d88ff9321ab39eca29003bf7
Zexian <zexianlie@gmail.com>
2025-12-19 07:40:49 -0800

    remove old entries and add script to package.json

--- seed/employees.seed.js
+++ seed/employees.seed.js
@@ -1,92 +1,91 @@
 // import mongoose from "mongoose";
-import { EMPLOYEE_IDS, HOUSE_IDS } from "../utils/ids.js";
-import Employee from "../models/Employee.js";
+import { EMPLOYEE_IDS, HOUSE_IDS } from '../utils/ids.js';
+import Employee from '../models/Employee.js';
 // import db connection
 // import the models
 
 export const seedEmployees = async () => {
-  try {
-    // optionally delete all data in user first
-    // await Employee.deleteMany();
+	try {
+		await Employee.deleteMany({});
 
-    await Employee.insertMany([
-      {
-        _id: EMPLOYEE_IDS.FIRST,
-        firstName: "Alice",
-        lastName: "Wu",
-        middleName: "Constance",
-        preferredName: "Alice",
-        profilePictureUrl:
-          "https://media.istockphoto.com/id/2022468311/vector/single-man-stick-figure-icon.jpg?s=612x612&w=0&k=20&c=_xCA1Y3pMvigQ9mejEAM56skgfAdBorLRqwiKckuTws=",
-        dateOfBirth: new Date("2000-06-29"),
-        gender: "Female",
-        workAuthorization: "F1(CPT/OPT)",
-        otherAuth: "", //what needs to be if it is not 'Other'
-        workAuthorizationStart: new Date("2024-03-08"),
-        workAuthorizationEnd: new Date("2029-03-08"),
-        carInformation: {
-          make: "Toyota",
-          model: "Civic",
-          color: "Silver",
-        },
-        driverLicense: {
-          number: "1234567890",
-          exp: new Date("2028-03-03"),
-        },
-        driverLicenseDoc:
-          "https://swiftmedia.s3.amazonaws.com/mountain.swiftcom.com/images/sites/5/2016/08/11143810/Codriverslicense-atd-030116.jpg",
-        cellPhone: "1234567890",
-        workPhone: "0987654321",
-        address: {
-          num: "",
-          street: "12345 67th Ave W",
-          city: "Seattle",
-          state: "Washington",
-          zip: "98012",
-        },
-        references: [
-          {
-            firstName: "Manny",
-            lastName: "Pacquiao",
-            relationship: "relational",
-            phone: "1029384756",
-            email: "firstref@domain.com",
-          },
-          {
-            firstName: "Floyd",
-            lastName: "Mayweather",
-            relationship: "relational",
-            phone: "1029384756",
-            email: "secondref@domain.com",
-          },
-        ],
-        ssn: "12345678",
-        emergencyContacts: [
-          {
-            firstName: "first",
-            lastName: "emergency",
-            middleName: "middle",
-            phone: "21548751849",
-            email: "firstemerg@domain.com",
-            relationship: "siblings",
-          },
-          {
-            firstName: "second",
-            lastName: "emergency",
-            middleName: "middle",
-            phone: "1452469109",
-            email: "secondemerg@domain.com",
-            relationship: "parent",
-          },
-        ],
-        houseId: HOUSE_IDS.FIRST,
-        // currDocumentId and onboarding I set to null
-      },
-    ]);
+		await Employee.insertMany([
+			{
+				_id: EMPLOYEE_IDS.FIRST,
+				firstName: 'Alice',
+				lastName: 'Wu',
+				middleName: 'Constance',
+				preferredName: 'Alice',
+				profilePictureUrl:
+					'https://media.istockphoto.com/id/2022468311/vector/single-man-stick-figure-icon.jpg?s=612x612&w=0&k=20&c=_xCA1Y3pMvigQ9mejEAM56skgfAdBorLRqwiKckuTws=',
+				dateOfBirth: new Date('2000-06-29'),
+				gender: 'Female',
+				workAuthorization: 'F1(CPT/OPT)',
+				otherAuth: '', //what needs to be if it is not 'Other'
+				workAuthorizationStart: new Date('2024-03-08'),
+				workAuthorizationEnd: new Date('2029-03-08'),
+				carInformation: {
+					make: 'Toyota',
+					model: 'Civic',
+					color: 'Silver',
+				},
+				driverLicense: {
+					number: '1234567890',
+					exp: new Date('2028-03-03'),
+				},
+				driverLicenseDoc:
+					'https://swiftmedia.s3.amazonaws.com/mountain.swiftcom.com/images/sites/5/2016/08/11143810/Codriverslicense-atd-030116.jpg',
+				cellPhone: '1234567890',
+				workPhone: '0987654321',
+				address: {
+					num: '',
+					street: '12345 67th Ave W',
+					city: 'Seattle',
+					state: 'Washington',
+					zip: '98012',
+				},
+				references: [
+					{
+						firstName: 'Manny',
+						lastName: 'Pacquiao',
+						relationship: 'relational',
+						phone: '1029384756',
+						email: 'firstref@domain.com',
+					},
+					{
+						firstName: 'Floyd',
+						lastName: 'Mayweather',
+						relationship: 'relational',
+						phone: '1029384756',
+						email: 'secondref@domain.com',
+					},
+				],
+				ssn: '12345678',
+				emergencyContacts: [
+					{
+						firstName: 'first',
+						lastName: 'emergency',
+						middleName: 'middle',
+						phone: '21548751849',
+						email: 'firstemerg@domain.com',
+						relationship: 'siblings',
+					},
+					{
+						firstName: 'second',
+						lastName: 'emergency',
+						middleName: 'middle',
+						phone: '1452469109',
+						email: 'secondemerg@domain.com',
+						relationship: 'parent',
+					},
+				],
+				houseId: HOUSE_IDS.FIRST,
+				// currDocumentId and onboarding I set to null
+			},
+		]);
 
-    // process.exit(0);
-  } catch (err) {
-    console.error(err);
-    // process.exit(1);
-  }
+		// process.exit(0);
+	} catch (err) {
+		console.error(err);
+		// process.exit(1);
+	}
 };
--- seed/houses.seed.js
+++ seed/houses.seed.js
@@ -1,45 +1,44 @@
 // import mongoose from "mongoose";
-import { HOUSE_IDS } from "../utils/ids.js";
-import House from "../models/House.js";
+import { HOUSE_IDS } from '../utils/ids.js';
+import House from '../models/House.js';
 // import db connection
 // import the models
 
 export const seedHouses = async () => {
-  try {
-    // optionally delete all data in user first
-    // await House.deleteMany();
+	try {
+		await House.deleteMany({});
 
-    await House.insertMany([
-      {
-        _id: HOUSE_IDS.FIRST,
-        address: {
-          num: "",
-          street: "76543 21th Ave N",
-          city: "Seattle",
-          state: "Washington",
-          zip: "98034",
-        },
-        landlord: {
-          fullName: "Landlord Full Name",
-          phoneNumber: "1234567898765",
-          email: "landlord@gmail.com",
-        },
-        utilities: {
-          bedrooms: 4,
-          bathrooms: 2,
-          mattresses: 4,
-          tables: 4,
-          chairs: 4,
-        },
-        residentEmployeeIds: [], // array of employee's foreign key
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      },
-    ]);
+		await House.insertMany([
+			{
+				_id: HOUSE_IDS.FIRST,
+				address: {
+					num: '',
+					street: '76543 21th Ave N',
+					city: 'Seattle',
+					state: 'Washington',
+					zip: '98034',
+				},
+				landlord: {
+					fullName: 'Landlord Full Name',
+					phoneNumber: '1234567898765',
+					email: 'landlord@gmail.com',
+				},
+				utilities: {
+					bedrooms: 4,
+					bathrooms: 2,
+					mattresses: 4,
+					tables: 4,
+					chairs: 4,
+				},
+				residentEmployeeIds: [], // array of employee's foreign key
+				description:
+					"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+			},
+		]);
 
-    // process.exit(0);
-  } catch (err) {
-    console.error(err);
-    // process.exit(1);
-  }
+		// process.exit(0);
+	} catch (err) {
+		console.error(err);
+		// process.exit(1);
+	}
 };
