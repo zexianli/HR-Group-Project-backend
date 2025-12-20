e0318066333660f388136e0c6ac01c191ba84f4c
Yichen Lu <luyichen0501@gmail.com>
2025-12-20 14:01:44 -0800

    HP-19 Configure AWS S3 for File Storage minor fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import 'dotenv/config';
 import app from './app.js';
-// import { connectDB } from './config/db.js';
+import { connectDB } from './config/db.js';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 async function startServer() {
-  // await connectDB(MONGODB_URI);
+  await connectDB(MONGODB_URI);
 
   app.listen(3000, () => console.log(`http://localhost:3000`));
 }
