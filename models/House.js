6f55f2894fddd551cf071da9dae4e24c4c364b0d
Zexian <zexianlie@gmail.com>
2025-12-22 19:09:12 -0800

    update house schema reference to EmployeeProfile

--- models/House.js
+++ models/House.js
@@ -1,48 +1,51 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const AddressSchema = new mongoose.Schema(
-    {
-        num: { type: String, default: "" },
-        street: { type: String, default: "" },
-        city: { type: String, default: "" },
-        state: { type: String, default: "" },
-        zip: { type: String, default: "" },
-    },
-    { _id: false }
+  {
+    num: { type: String, default: '' },
+    street: { type: String, default: '' },
+    city: { type: String, default: '' },
+    state: { type: String, default: '' },
+    zip: { type: String, default: '' },
+  },
+  { _id: false }
 );
 
 const LandlordSchema = new mongoose.Schema(
-    {
-        fullName: { type: String, default: "" },
-        phoneNumber: { type: String, default: "" },
-        email: { type: String, default: "" },
-    },
-    { _id: false }
+  {
+    fullName: { type: String, default: '' },
+    phoneNumber: { type: String, default: '' },
+    email: { type: String, default: '' },
+  },
+  { _id: false }
 );
 
 const UtilitiesSchema = new mongoose.Schema(
-    {
-        bedrooms: { type: Number, default: 0 },
-        bathrooms: { type: Number, default: 0 },
-        mattresses: { type: Number, default: 0 },
-        tables: { type: Number, default: 0 },
-        chairs: { type: Number, default: 0 },
-    },
-    { _id: false }
+  {
+    bedrooms: { type: Number, default: 0 },
+    bathrooms: { type: Number, default: 0 },
+    mattresses: { type: Number, default: 0 },
+    tables: { type: Number, default: 0 },
+    chairs: { type: Number, default: 0 },
+  },
+  { _id: false }
 );
 
 const HouseSchema = new mongoose.Schema(
-    {
-        _id: { type: String, required: true },
-        address: { type: AddressSchema, default: () => ({}) },
-        landlord: { type: LandlordSchema, default: () => ({}) },
-        utilities: { type: UtilitiesSchema, default: () => ({}) },
+  {
+    address: { type: AddressSchema, default: () => ({}) },
+    landlord: { type: LandlordSchema, default: () => ({}) },
+    utilities: { type: UtilitiesSchema, default: () => ({}) },
 
-        residentEmployeeIds: { type: [String], default: [] },
-
-        description: { type: String, default: "" },
+    residentEmployeeIds: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'EmployeeProfile',
+      default: [],
     },
-    { timestamps: true }
+
+    description: { type: String, default: '' },
+  },
+  { timestamps: true }
 );
 
-export default mongoose.model("House", HouseSchema);
+export default mongoose.model('House', HouseSchema);
