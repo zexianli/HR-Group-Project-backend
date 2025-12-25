55ca9ca1d7323bd2f53e93837e41d7c01b2628ba
Zexian <zexianlie@gmail.com>
2025-12-24 20:07:51 -0800

    endpoint to add comment to report

--- routes/houseRoutes.js
+++ routes/houseRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { authenticate, employeeOnly } from '../middlewares/auth.js';
-import { getAssignedHouse, createFacilityReport } from '../controllers/houseController.js';
+import {
+  getAssignedHouse,
+  createFacilityReport,
+  addCommentToReport,
+} from '../controllers/houseController.js';
 
 const router = express.Router();
 
 router.get('/housing/me', authenticate, employeeOnly, getAssignedHouse);
 router.post('/housing/reports', authenticate, employeeOnly, createFacilityReport);
+router.post('/housing/reports/:id/comments', authenticate, addCommentToReport);
 
 export default router;
