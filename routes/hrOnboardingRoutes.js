8e1751d93e23829b815edab8327c58809f482063
ALLEN0607 <124327793+ALLEN0607@users.noreply.github.com>
2025-12-28 22:17:48 -0800

    HP-75/HP-87: add HR onboarding list, detail, approve and reject APIs (#47)

--- routes/hrOnboardingRoutes.js
+++ routes/hrOnboardingRoutes.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import { authenticate, hrOnly } from '../middlewares/auth.js';
 import {
+  getOnboardingApplications,
+  getOnboardingApplicationById,
   approveOnboadingApplication,
   rejectOnboardingApplication,
 } from '../controllers/hrOnboardingController.js';
 
 const router = express.Router();
 
-router.put('/onboarding/:id/approve', authenticate, hrOnly, approveOnboadingApplication);
-router.put('/onboarding/:id/reject', authenticate, hrOnly, rejectOnboardingApplication);
+router.use(authenticate, hrOnly);
+
+// HP-75
+router.get('/onboarding', getOnboardingApplications);
+router.get('/onboarding/:id', getOnboardingApplicationById);
+
+// HP-87
+router.put('/onboarding/:id/approve', approveOnboadingApplication);
+router.put('/onboarding/:id/reject', rejectOnboardingApplication);
 
 export default router;
